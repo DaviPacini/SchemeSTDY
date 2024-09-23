fcc031e85ba885eaa356ea38234689dbc081c27d
Davi Pacini <126252677+DaviPacini@users.noreply.github.com>
2024-09-23 17:32:59 -0300

    Add files via upload

--- teste.scm
+++ teste.scm
@@ -1,13 +1,31 @@
 #lang scheme
- teste de if
+;teste de if
 (define x 10)
 (if (< x 11)
     (display "Im a small number")
     (display "Im a big number")
  )
+(newline)
 
 ;listas
 (define a '("nome" "idade" "ano")); Define a lista com nome "a"
 (display a) ;Simplifica a lista
 (newline)
 (write a) ;Preserva as aspas
+(newline)
+
+;função de primeira classe
+(define (funcao x y)
+  (x y))
+(define (lado_quadrado z)
+  (* z z))
+(display "Area do quadrado de lado 3: ")
+(funcao lado_quadrado 3)
+(newline)
+
+;leitura de entrada
+(display "informe o valor do lado do quadrado: ")
+(define entrada (read))
+(newline)
+(funcao lado_quadrado entrada)
+(newline)
